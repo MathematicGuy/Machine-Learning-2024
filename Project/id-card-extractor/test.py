c9ed7e64f22d022d376e782b1cc13302bc8ca35d
Sukmadi <54486758+MathematicGuy@users.noreply.github.com>
2024-12-11 16:51:39 +0700

    Mark subprojects as dirty in TFODCourse and DetectAnomalyPCB

--- Project/id-card-extractor/test.py
+++ Project/id-card-extractor/test.py
@@ -1,34 +1,46 @@
-from ultralytics import YOLO
 import cv2
 from PIL import Image
+from skimage import exposure, util
+from skimage.morphology import disk
+from skimage.filters import rank
+from ultralytics import YOLO
 
 # Load a model
 model = YOLO("weights/best.pt")
 
 # Open a video capture
 cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error: Could not open video capture.")
+    exit()
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+try:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Could not read frame.")
+            break
 
-    # Convert the frame to grayscale
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Convert the frame to grayscale
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Convert grayscale image back to BGR format for YOLO model
-    gray_frame_bgr = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2BGR)
+        # Apply histogram equalization
+        img_rescale = exposure.equalize_hist(gray_frame)
+        img_rescale_uint8 = util.img_as_ubyte(img_rescale)
+        img_eq = rank.equalize(img_rescale_uint8, footprint=disk(25))  # Reduced disk size
 
-    # Perform object detection on the grayscale image
-    results = model.predict(source=gray_frame_bgr, show=True)
+        # Convert equalized grayscale image back to BGR format for YOLO model
+        gray_frame_bgr = cv2.cvtColor(img_eq, cv2.COLOR_GRAY2BGR)
 
-    # Display the resulting frame
-    cv2.imshow('frame', gray_frame_bgr)
+        # Perform object detection on the grayscale image
+        results = model.predict(source=gray_frame_bgr, show=True)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Display the resulting frame
+        cv2.imshow('frame', gray_frame_bgr)
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
-
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+finally:
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
