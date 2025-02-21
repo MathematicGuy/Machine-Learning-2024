8ee3e53c3d31b1c6e2bb85ff289b5c7bc9ba48fb
Sukmadi <54486758+MathematicGuy@users.noreply.github.com>
2025-02-21 07:05:28 +0700

    sentiment analysis with LSTM

--- Learn/ComputerVision/test.py
+++ Learn/ComputerVision/test.py
@@ -3,5 +3,10 @@ path = pathlib.Path('Morphological')
 pathglob = list(path.glob('*'))
 # print(pathglob)
 
-pathiter = next(path.iterdir(), 2)
-print(pathiter)
+print(next(path.iterdir(),"Hey There"))
+
+numbers = iter([1, 2, 3])
+print(next(numbers, 'No more items'))  # Output: 1
+print(next(numbers, 'No more items'))  # Output: 2
+print(next(numbers, 'No more items'))  # Output: 3
+print(next(numbers, 'No more items'))  # Output: No more
